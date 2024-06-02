a716f0bbe2b46034727243ab02afdfe2b990ed34
Pablo Villalobos <pablo.villalobosdonoso@gmail.com>
2024-06-01 22:09:15 -0400

    Se corrigio diseño

--- app/src/main/java/com/example/valpotours/RegistroActivity.kt
+++ app/src/main/java/com/example/valpotours/RegistroActivity.kt
@@ -1,7 +1,10 @@
 package com.example.valpotours
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.textclassifier.TextLinks.TextLink
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +17,16 @@ class RegistroActivity : AppCompatActivity() {
     private lateinit var btnBack:Button
     private lateinit var tvWelcome:TextView
     private lateinit var cvFullName:CardView
-
-
+    private lateinit var etFullName:EditText
+    private lateinit var cvEmail:CardView
+    private lateinit var etEmail:EditText
+    private lateinit var cvPassword:CardView
+    private lateinit var etPassword:EditText
+    private lateinit var cvConfirmPassword:CardView
+    private lateinit var etConfirmPassword:EditText
+    private lateinit var btnRegistrar:Button
+    private lateinit var tvSignIn:TextView
+    private lateinit var tvSignInLink:TextLink
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +38,29 @@ class RegistroActivity : AppCompatActivity() {
             insets
         }
         initComponents()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btnBack.setOnClickListener {
+            var intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun initComponents() {
-        TODO("Not yet implemented")
+        btnBack = findViewById(R.id.btnBack)
+        tvWelcome = findViewById(R.id.tvWelcome)
+        cvFullName = findViewById(R.id.cvFullName)
+        etFullName = findViewById(R.id.etFullName)
+        cvEmail = findViewById(R.id.cvEmail)
+        etEmail = findViewById(R.id.etEmail)
+        cvPassword = findViewById(R.id.cvPassword)
+        etPassword = findViewById(R.id.etPassword)
+        cvConfirmPassword = findViewById(R.id.cvConfirmPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+        tvSignIn = findViewById(R.id.tvSignIn)
+        tvSignInLink = findViewById<TextLink>(R.id.tvSignInLink)
     }
 }
