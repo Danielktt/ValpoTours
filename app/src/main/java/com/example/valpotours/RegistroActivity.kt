0bebd1ac845f1faf6a51aa4d9b53bc742d71e6c9
Pablo Villalobos <pablo.villalobosdonoso@gmail.com>
2024-06-01 22:09:15 -0400

    Se corrigio diseño

--- app/src/main/java/com/example/valpotours/RegistroActivity.kt
+++ app/src/main/java/com/example/valpotours/RegistroActivity.kt
@@ -2,27 +2,31 @@ package com.example.valpotours
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.textclassifier.TextLinks.TextLink
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
-import kotlin.properties.Delegates
 
 class RegistroActivity : AppCompatActivity() {
 
-    private lateinit var etFullName: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var etPassword2: EditText
-
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var btnBack:Button
+    private lateinit var tvWelcome:TextView
+    private lateinit var cvFullName:CardView
+    private lateinit var etFullName:EditText
+    private lateinit var cvEmail:CardView
+    private lateinit var etEmail:EditText
+    private lateinit var cvPassword:CardView
+    private lateinit var etPassword:EditText
+    private lateinit var cvConfirmPassword:CardView
+    private lateinit var etConfirmPassword:EditText
+    private lateinit var btnRegistrar:Button
+    private lateinit var tvSignIn:TextView
+    private lateinit var tvSignInLink:TextLink
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,75 +37,30 @@ class RegistroActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        etFullName = findViewById(R.id.etFullName)
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
-        etPassword2 = findViewById(R.id.etConfirmPassword)
-
-        mAuth = FirebaseAuth.getInstance()
-
         initComponents()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btnBack.setOnClickListener {
+            var intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun initComponents() {
-        val btnRegister: Button = findViewById(R.id.BtnRegistrar)
-        btnRegister.setOnClickListener {
-            registerUser()
-        }
-    }
-
-    private fun registerUser() {
-        val fullName = etFullName.text.toString()
-        val email = etEmail.text.toString()
-        val password = etPassword.text.toString()
-        val password2 = etPassword2.text.toString()
-
-        if (fullName.isEmpty() || email.isEmpty() || password.isEmpty() || password2.isEmpty()) {
-            Toast.makeText(this, "Por favor, completa todos los campos.", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if (password != password2) {
-            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = mAuth.currentUser
-                    user?.let {
-                        val profileUpdates = UserProfileChangeRequest.Builder()
-                            .setDisplayName(fullName)
-                            .build()
-
-                        it.updateProfile(profileUpdates)
-                            .addOnCompleteListener { profileTask ->
-                                if (profileTask.isSuccessful) {
-                                    goHome(email)
-                                } else {
-                                    Toast.makeText(
-                                        this,
-                                        "Algo ha salido mal al actualizar el perfil",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-                    }
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Algo ha salido mal al registrarse: ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
-
-    private fun goHome(email: String) {
-        val intent = Intent(this, SettingsActivity::class.java)
-        startActivity(intent)
-        finish()
+        btnBack = findViewById(R.id.btnBack)
+        tvWelcome = findViewById(R.id.tvWelcome)
+        cvFullName = findViewById(R.id.cvFullName)
+        etFullName = findViewById(R.id.etFullName)
+        cvEmail = findViewById(R.id.cvEmail)
+        etEmail = findViewById(R.id.etEmail)
+        cvPassword = findViewById(R.id.cvPassword)
+        etPassword = findViewById(R.id.etPassword)
+        cvConfirmPassword = findViewById(R.id.cvConfirmPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+        tvSignIn = findViewById(R.id.tvSignIn)
+        tvSignInLink = findViewById<TextLink>(R.id.tvSignInLink)
     }
 }
