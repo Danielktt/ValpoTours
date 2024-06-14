68a1439522ccd9f1ac2b28ec9bcd7ccd42c0571e
Pablo Villalobos <pablo.villalobosdonoso@gmail.com>
2024-06-14 06:01:18 -0400

    se corrigieron errores en cambiar datos

--- app/src/main/java/com/example/valpotours/ChangeDatoActivity.kt
+++ app/src/main/java/com/example/valpotours/ChangeDatoActivity.kt
@@ -4,86 +4,69 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isVisible
+import com.example.valpotours.LoginActivity.Companion.userMail
 import com.example.valpotours.MainActivity.Companion.idUser
 import com.example.valpotours.databinding.ActivityChangeDatoBinding
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.SetOptions
 
 class ChangeDatoActivity : AppCompatActivity() {
-    private lateinit var etFullName: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var etPassword2: EditText
-    private lateinit var btnChangeDatos: Button
-
     private lateinit var mAuth: FirebaseAuth
-
     private lateinit var binding: ActivityChangeDatoBinding
     private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_dato)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         binding = ActivityChangeDatoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnCambiarDatos.setOnClickListener{
+
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.btnCambiarDatos.setOnClickListener {
             updateUserData()
         }
     }
 
-
-
     private fun updateUserData() {
-        val currentUser = idUser
+        val currentUser = mAuth.currentUser
         Log.i("usuarioes ", "Current user: $currentUser")
 
         if (currentUser != null) {
             val newFullName = binding.etFullName.text.toString().trim()
+            val currentPassword = binding.etCurrentPassword.text.toString().trim()
             val newPassword = binding.etPassword.text.toString().trim()
             val confirmPassword = binding.etConfirmPassword.text.toString().trim()
 
-            val userDocRef = db.collection("usuario").document(currentUser)
-            val updates = mutableMapOf<String, Any>()
+            if (currentPassword.isNotEmpty() && newPassword.isNotEmpty() && newPassword == confirmPassword) {
+                val credential = EmailAuthProvider.getCredential(currentUser.email!!, currentPassword)
 
-            if (newFullName.isNotEmpty()) {
-                updates["nombre"] = newFullName
-            }
-
-            if (newPassword.isNotEmpty() && newPassword == confirmPassword) {
-                updates["contraseña"] = newPassword  // Aquí debes ajustar el campo correspondiente en Firestore
+                currentUser.reauthenticate(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "Reauthentication successful.")
+                            currentUser.updatePassword(newPassword)
+                                .addOnCompleteListener { updateTask ->
+                                    if (updateTask.isSuccessful) {
+                                        Log.d(TAG, "User password updated.")
+                                        updateFirestoreUserData(newFullName)
+                                    } else {
+                                        Log.w(TAG, "Error updating password", updateTask.exception)
+                                        Toast.makeText(this, "Error al actualizar la contraseña", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                        } else {
+                            Log.w(TAG, "Reauthentication failed", task.exception)
+                            Toast.makeText(this, "Error de re-autenticación: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             } else if (newPassword.isNotEmpty() && newPassword != confirmPassword) {
                 Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
-                return
-            }
-
-            if (updates.isNotEmpty()) {
-                userDocRef.update(updates)
-                    .addOnSuccessListener {
-                        Log.d(TAG, "DocumentSnapshot successfully updated!")
-                        Toast.makeText(this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w(TAG, "Error updating document", e)
-                        Toast.makeText(this, "Error al actualizar datos", Toast.LENGTH_SHORT).show()
-                    }
             } else {
-                Toast.makeText(this, "No se han ingresado datos para actualizar", Toast.LENGTH_SHORT).show()
+                updateFirestoreUserData(newFullName)
             }
         } else {
             Log.w(TAG, "No user logged in")
@@ -91,6 +74,56 @@ class ChangeDatoActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateFirestoreUserData(newFullName: String) {
+        val userDocRef = db.collection("usuario").document(idUser)
+        val updates = mutableMapOf<String, Any>()
 
+        if (newFullName.isNotEmpty()) {
+            updates["nombre"] = newFullName
+        }
+        Log.i("PedroEsparrago", "${idUser}  ${newFullName}")
+        userDocRef.get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    if (updates.isNotEmpty()) {
+                        userDocRef.update(updates)
+                            .addOnSuccessListener {
+                                Log.d(TAG, "DocumentSnapshot successfully updated!")
+                                Toast.makeText(this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show()
+                                navigateToLogin()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error updating document", e)
+                                Toast.makeText(this, "Error al actualizar datos", Toast.LENGTH_SHORT).show()
+                            }
+                    } else {
+                        Toast.makeText(this, "No se han ingresado datos para actualizar", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    if (updates.isNotEmpty()) {
+                        userDocRef.set(updates, SetOptions.merge())
+                            .addOnSuccessListener {
+                                Log.d(TAG, "DocumentSnapshot successfully created!")
+                                Toast.makeText(this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show()
+                                navigateToLogin()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error creating document", e)
+                                Toast.makeText(this, "Error al crear datos", Toast.LENGTH_SHORT).show()
+                            }
+                    } else {
+                        Toast.makeText(this, "No se han ingresado datos para actualizar", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error fetching document", e)
+                Toast.makeText(this, "Error al obtener datos del usuario", Toast.LENGTH_SHORT).show()
+            }
+    }
 
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
 }
